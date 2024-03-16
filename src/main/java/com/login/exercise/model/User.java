97a63c7f2f1f33639a185fb89dc2382ae17be0e4
Josh Hurley <jrhurley5333@gmail.com>
2024-03-16 16:37:53 -0400

    init commit

---
+++ src/main/java/com/login/exercise/model/User.java
@@ -0,0 +1,27 @@
+package com.login.exercise.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private boolean enabled;
+    
+    
+}
